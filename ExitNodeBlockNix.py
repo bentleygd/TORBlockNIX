5e5df14f8a945e8d43bdfbc05dd4a14ad3fdacb5
root_secutil-prd-01 <root@secutil-prd-01.24hourfit.com>
2019-01-24 14:48:25 -0800

    Committer: bentleygd
    On branch master
    Changes to be committed:
    	renamed:    ExitNodeBlock.py -> ExitNodeBlockList.py
    	new file:   ExitNodeBlockNix.py

---
+++ ExitNodeBlockNix.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import stem.descriptor.remote
+from re import match
+from subprocess import popen
+
+
+# VARIABLE DECLARATION
+dbl_path = '/var/www/html/DBL/TOR.txt'
+ip_pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+tor_exit_list = []
+block_list = []
+
+# Connecting to TOR's directory servers and looking for all exit relays.  If
+# the relay is an exit relay, we are going to store the IP address in a list.
+
+for desc in stem.descriptor.remote.get_server_descriptors():
+    if desc.exit_policy.is_exiting_allowed():
+        tor_exit_list.append(desc.address)
+
+# Sort the list to make it neat, and for ease of comparison if that becomes
+# a desired feature later.  After we validate whethor not each entry is an
+# actual IP address, we write the IP addresses as a set to a file to remove
+# duplicates.  Note that write mode (as opposed to append) is being used,
+# which means that the file will be overwritten each time the script runs.
+tor_exit_list.sort()
+
+for ip_addr in set(tor_exit_list):
+    if match(ip_pattern, ip_addr):
+        popen('/usr/bin/echo' + ip_addr)
