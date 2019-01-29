9e4bcbaf63dd4700794c987fc1aba9ff375f2ed7
root_secutil-prd-01 <root@secutil-prd-01.24hourfit.com>
2019-01-29 14:37:53 -0800

    modified:   ExitNodeBlockNix.py

--- ExitNodeBlockNix.py
+++ ExitNodeBlockNix.py
@@ -1,13 +1,68 @@
 #!/usr/bin/python
 import stem.descriptor.remote
-from re import match
-from subprocess import Popen
+from re import match, search, split
+from subprocess import check_output, CalledProcessError, STDOUT, Popen
+from sys import exc_info
+from time import sleep
+
+
+# This function sets up iptables by creating a specific chain to block 
+# TOR exit nodes.  It also modifies the INPUT chain to jump to this chain
+# first and sets the last rule of this chain to jump back to INPUT.
+
+def TORChainSetup():
+# Create the iptables chain for blocking TOR exit relays.
+    try:
+        check_output('/sbin/iptables -N TOR-BLOCK', shell=True, sterr=STDOUT)
+    except CalledProcessError as CreateChain:
+        if match('iptables: Chain already exists\.', CreateChain.output):
+            pass
+        if search('Permssion Denied', CreateChain.output):
+            print 'Error:', CreateChain.output
+            print 'Try executing the script as root.'
+            sys.exit(1)
+        else:
+            print 'The error given by iptables is:', CreateChain.output
+            print 'Error:', exc_info[1]
+
+# Obtain a list of iptables rules for the TOR-BLOCK chain.      
+    list_block_chain = split('\n', check_output('/sbin/iptables -L TOR-BLOCK',
+                              shell=True, stderr=STDOUT))
+
+# Check to see if there is a return rule, and if there isn't create one.
+    for entry in list_block_chain:
+        if search('^RETURN', entry):
+            pass
+        else:
+            Popen('/sbin/iptables -A -j RETURN', shell=True)
+
+# Create a count of iptables rules that are not the return rule in the
+# TOR-BLOCK chain.  Once we have this we will iterate a process toe delete 
+# all the rules that have a range between 1 and n, where n equals the number
+# of iptables rules that are not a return rule.
+    block_chain_num = 0
+    for entry in list_block_chain:
+        if search('^RETURN', entry):
+            pass
+        else:
+            block_chain_num = block_chain num + 1
+
+    del_num = block_chain_num + 1
+    del counter = 0
+    for rule_num in range(1, del_num):
+        if del_counter <= 10:
+            Popen('/sbin/iptables -D ' + str(del_num) + 'TOR-BLOCK',
+                  shell=True)
+        else:
+            del_counter = 0
+            sleep(1)
 
 
 # VARIABLE DECLARATION
 ip_pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 tor_exit_list = []
 block_list = []
+
 
 # Connecting to TOR's directory servers and looking for all exit relays.  If
 # the relay is an exit relay, we are going to store the IP address in a list.
@@ -23,4 +78,4 @@ tor_exit_list.sort()
 
 for ip_addr in set(tor_exit_list):
     if match(ip_pattern, ip_addr):
-        Popen('/usr/bin/echo' + ip_addr)
+        block_list.append(ip_addr)
