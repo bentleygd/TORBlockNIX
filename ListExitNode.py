ead4d20c5ac5a8cbe1a807f734ecaf51be9fd907
bentleygd <gbentley@24hourfit.com>
2020-04-06 15:52:44 -0700

    Code overhaul

--- ListExitNode.py
+++ ListExitNode.py
@@ -1,20 +1,74 @@
 #!/usr/bin/python
 import stem.descriptor.remote
 from re import match
+from logging import basicConfig, getLogger, INFO
 
 
-# VARIABLE DECLARATION
-file_path = '/path/to/output.txt'
-tor_exit_list = []
+def validate_ip(ip_addr):
+    """Takes a string input and returns true if it is a valid IP."""
+    valid_ip = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    if match(valid_ip, ip_addr):
+        octets = ip_addr.split('.')
+        if (int(octets[0]) <= 223 and
+                int(octets[1]) <= 255 and
+                int(octets[2]) <= 255 and
+                int(octets[3]) <= 254):
+            return True
+        else:
+            raise ValueError
+    else:
+        raise ValueError
 
-# Obtain a list of exit nodes and write them to a file after verifying
-# that each entry is an IP addres.
-for desc in stem.descriptor.remote.get_server_descriptors():
-    if desc.exit_policy.is_exiting_allowed():
-        tor_exit_list.append(desc.address)
-tor_exit_list.sort()
-exit_node_file = open(file_path, 'w')
-for ip_addr in set(tor_exit_list):
-    if match('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip_addr):
-        exit_node_file.write(ip_addr + '/32' + '\n')
-exit_node_file.close()
+
+def get_exit_relays():
+    """Connects to TOR and returns a list of exit relays.
+
+    Keyword Arguments:
+    None
+
+    Outputs:
+    tor_exit_list - A list of exit relays.
+
+    Raises:
+    TBD."""
+    tor_exit_list = []
+    log = getLogger('tor_exit_log')
+    try:
+        for desc in stem.descriptor.remote.get_server_descriptors():
+            if (desc.exit_policy.is_exiting_allowed() and
+                    validate_ip(desc.address)):
+                tor_exit_list.append(desc.address)
+    except ValueError:
+        log.error(
+            'Exit relay address did not pass input validation', exc_info=1
+        )
+    except Exception:
+        log.error('Error occurred retrieving exit relays.', exc_info=1)
+        exit(1)
+    return tor_exit_list
+
+
+def main():
+    """Doing the thing."""
+    log = getLogger('tor_exit_log')
+    basicConfig(
+        format='%(asctime)s %(name)s %(levelname)s: %(message)s',
+        datefmt='%m/%d/%Y %H:%M:%S',
+        level=INFO,
+        filename='tor_exit_log.log'
+    )
+    try:
+        exit_node_file = open('tor_list.txt', 'w', encoding='ascii')
+    except OSError:
+        log.error('Unable to open exit relay results file.', excinfo=1)
+    log.info('Retrieving list of exit relays from the TOR mirrors.')
+    exit_relays = get_exit_relays()
+    log.debug('Writing exit relays to file.')
+    for relay in set(exit_relays):
+        exit_node_file.write(relay + '/32' + '\n')
+    exit_node_file.close()
+    log.info('Completed attempt to retrieve exit relays.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_cases.py
@@ -0,0 +1,52 @@
+from TORBlockNIX.ListExitNode import get_exit_relays
+from os.path import exists
+from os import remove
+
+
+class TestTORStuff:
+    """A class for testing retrieving things via the stem module.
+
+    Keyword aguments:
+    None
+
+    Methods:
+    test_exit_list - Tests connecting to TOR and populating a list of exit
+    relays.
+    """
+
+    def test_exit_list(self):
+        """Tests retrieving a list of exit relays."""
+        tor_exit_list = get_exit_relays()
+        assert len(tor_exit_list) > 300
+
+
+class TestUserPrivs:
+    """A class for testing permissions of current user.
+
+    Keyword arguments:
+    None.
+
+    Methods:
+    test_file_create - Tests writing to files.
+    test_file_remove - Tests file deletion."""
+
+    def test_file_create(self):
+        """Tests writing to files."""
+        test_file_name = 'test_file.txt'
+        test_file = open(test_file_name, 'w', encoding='ascii')
+        test_file.write('Some string.')
+        test_file.close()
+        if exists(test_file_name):
+            test = True
+        else:
+            test = False
+        assert test is True
+    
+    def test_file_remove(self):
+        """Tests file deletion."""
+        test_file_name = 'test_file.txt'
+        remove(test_file_name)
+        if not exists(test_file_name):
+            test = True
+        else:
+            test = False
